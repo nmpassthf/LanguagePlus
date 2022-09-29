aa656f89af971a92c5b1535b844c0b3a2e171c43
NormalPassTHFan <nmpassthf@gmail.com>
2022-09-30 06:20:46 +0800

    ✨ 完成了lex模块

--- test/data/src.pl
+++ test/data/src.pl
@@ -1,4 +1,67 @@
-a = 10
-b = a+1
-echo a
+a = 10b = a+1echo a
 echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo a
+echo b
+b=a+aaa = 10b = a+1echo bbbbbbbbbsdss
+fsd
+fsdf
++ds
++fsddfsd
++fsddfsds
+a
+=
+ss
+sfsd
